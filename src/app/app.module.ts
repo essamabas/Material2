ebf1f9fba47e00712e23807db9701c91edaf1393
Abas <Essam.Abas@getrag.com>
2017-03-31 14:55:56 +0200

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import 'hammerjs';
+
+import {SettingsDialog} from "./settings/settings.Component"
+import { AppComponent} from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingsDialog
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,11 @@
+
+import { Component} from '@angular/core';
+import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
+
+@Component({
+  selector: 'settings-dialog',
+  templateUrl: './settings.component.html',
+})
+export class SettingsDialog {
+
+}
